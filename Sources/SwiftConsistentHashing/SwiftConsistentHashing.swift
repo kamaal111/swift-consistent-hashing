38690c897324165ebdefef119841b2563d10e5a4
Kamaal Farah <kamaal.f1@gmail.com>
2024-06-23 14:20:08 +0200

    Making use of swift crypto

--- Sources/SwiftConsistentHashing/SwiftConsistentHashing.swift
+++ Sources/SwiftConsistentHashing/SwiftConsistentHashing.swift
@@ -5,15 +5,18 @@
 //  Created by Kamaal M Farah on 23/06/2024.
 //
 
+import CryptoKit
 import Foundation
-import CommonCrypto
 
 public enum ConsistentHashRingHashingAlgorithms {
     case sha256
 
-    func hash(_ message: String) -> String? {
+    func hash(_ message: String) -> String {
         switch self {
-        case .sha256: SHA256.hash(message)
+        case .sha256: SHA256
+                .hash(data: message.data(using: .utf8)!)
+                .compactMap({ byte in String(format: "%02x", byte) })
+                .joined()
         }
     }
 }
@@ -36,29 +39,4 @@ public class ConsistentHashRing {
     public func removeNode() { }
 
     public func getNode() { }
-}
-
-private enum SHA256 {
-    static func hash(_ message: String) -> String? {
-        guard let encodedMessage = message.data(using: .utf8) else { return nil }
-
-        return hexStringFromData(input: digest(input: encodedMessage as NSData))
-    }
-
-    static private func digest(input : NSData) -> NSData {
-        let digestLength = Int(CC_SHA256_DIGEST_LENGTH)
-        var hash = [UInt8](repeating: 0, count: digestLength)
-        CC_SHA256(input.bytes, UInt32(input.length), &hash)
-
-        return NSData(bytes: hash, length: digestLength)
-    }
-
-    static private  func hexStringFromData(input: NSData) -> String {
-        var bytes = [UInt8](repeating: 0, count: input.length)
-        input.getBytes(&bytes, length: input.length)
-
-        return bytes.reduce("") { result, byte in
-            result + String(format:"%02x", UInt8(byte))
-        }
-    }
 }
